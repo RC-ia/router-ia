e24e9af1c30581d649d167ebbe55924d49da3418
RC <221199316+RC-ia@users.noreply.github.com>
2026-09-04 14:49:14 -0300

    Route canonical runner to fixed Q4 and physical RAM modules

--- src/router_ia/qwen36_chat_batch_runner.py
+++ src/router_ia/qwen36_chat_batch_runner.py
@@ -20,8 +20,8 @@ from . import qwen36_gpu_q4 as _gpu_q4  # noqa: F401
 from . import qwen36_fp16_expert_cache as _fp16_expert_cache  # noqa: F401
 from . import qwen36_expert_batch_plan_v2 as _expert_batch_plan_v2  # noqa: F401
 from . import qwen36_expert_batch_plan_v3 as _expert_batch_plan_v3  # noqa: F401
-from . import qwen36_expert_q4_hierarchy as _expert_q4_hierarchy  # noqa: F401
-from . import qwen36_physical_memory_guard as _physical_memory_guard  # noqa: F401
+from . import qwen36_expert_q4_hierarchy_fixed as _expert_q4_hierarchy  # noqa: F401
+from . import qwen36_physical_memory_guard_v2 as _physical_memory_guard  # noqa: F401
 from . import qwen36_memory_policy_v2 as _memory_policy_v2  # noqa: F401
 
 if os.getenv("QWEN36_PROFILE", "0").strip().lower() in {"1", "true", "yes", "on"}:
